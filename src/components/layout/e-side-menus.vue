979c4366441efd6b95df67eb5750f6969e27afb1
saullary <saullarytzc94@gmail.com>
2021-09-01 18:27:06 +0800

    chore: update side menus

--- src/components/layout/e-side-menus.vue
+++ src/components/layout/e-side-menus.vue
@@ -1,6 +1,5 @@
 <template>
   <div v-if="asMobile">
-    <!-- :value="0" -->
     <v-expansion-panels accordion v-model="curIdx">
       <v-expansion-panel v-for="(it, i) in list" :key="i">
         <v-expansion-panel-header>
@@ -17,14 +16,12 @@
       </v-expansion-panel>
     </v-expansion-panels>
   </div>
+
   <div class="d-flex" v-else>
     <div class="bdr-1">
       <v-list flat class="mt-3" min-width="240">
         <v-list-item-group v-model="curIdx" color="primary">
           <v-list-item v-for="(it, i) in list" :key="i">
-            <!-- <v-list-item-icon>
-            <v-icon v-text="item.icon"></v-icon>
-          </v-list-item-icon> -->
             <v-list-item-content>
               <v-list-item-title v-text="it.text"></v-list-item-title>
             </v-list-item-content>
