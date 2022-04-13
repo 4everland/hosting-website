5ab36a15d588e13d0217ec492e1060a88af223f5
hqs <imhuangqs@gmail.com>
2022-04-13 16:25:47 +0800

    chore: update end time

--- src/pages/dashboard/act-first-landing.vue
+++ src/pages/dashboard/act-first-landing.vue
@@ -519,7 +519,7 @@ export default {
       if (this.noChange) {
         let tip = this.myEthAddr;
         if (!this.myEthAddr) {
-          tip = "Unable to set wallet adress after 21st October.";
+          tip = "Unable to set wallet adress after 21st October 2021.";
         } else if (this.errAccount) {
           tip = `Wallet address(${this.ethAddr.cutStr(
             6,
